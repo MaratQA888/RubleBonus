97114a742a3debefc21659971325fda5923e7230
MaratQA888 <yennifreak@icloud.com>
2022-12-24 14:24:22 +0300

    RenamingOf

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int startmoney = 100;
+        int startMoney = 100;
         int purchased = 1100;
         if (purchased > 1000) {
             System.out.println("Бонусов начислено:");
             System.out.println(purchased / 100);
             System.out.println("Итоговый баланс:");
-            System.out.println(purchased / 100 + startmoney + purchased);// Объявляете переменные для входных данных и
+            System.out.println(purchased / 100 + startMoney + purchased);// Объявляете переменные для входных данных и
         } else {
             System.out.println("Ваш баланс:");
-            System.out.println(startmoney + purchased);// параметров программы: начального счёта,
+            System.out.println(startMoney + purchased);// параметров программы: начального счёта,
         }
         // суммы пополнения и тп
 
